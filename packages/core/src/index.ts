f51e4abe26fbc55056ca8cdc7ddeecc257815c37
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-04-05 00:05:27 +0200

    feat: request render to core export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,3 +5,4 @@ export { property } from "./property";
 export { customElement } from "./customElement";
 export { publish, subscribe, unSubscribe, publishNext, publishSync } from "./transmitter";
 export { validateKey, stateContainer, stateResult } from "./state";
+export { requestRender } from "./requestRender";
