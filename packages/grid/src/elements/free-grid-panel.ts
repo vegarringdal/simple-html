19592ee31d148106ad5e79e2feb8667db55eb331
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-04-04 23:13:05 +0200

    add panel groups (dead items atm)

--- packages/grid/src/elements/free-grid-panel.ts
+++ packages/grid/src/elements/free-grid-panel.ts
@@ -1,6 +1,6 @@
 import { customElement } from '@simple-html/core';
 import { GridInterface } from '../gridInterface';
-import { html } from 'lit-html';
+import { html, svg } from 'lit-html';
 
 @customElement('free-grid-panel')
 export default class extends HTMLElement {
@@ -13,8 +13,47 @@ export default class extends HTMLElement {
     }
 
     render() {
+        const grouping = this.connector.config.groupingSet || [];
+        
+        const mouseEnter = (e: MouseEvent) => {
+            (<HTMLElement>e.target)
+                .getElementsByClassName('free-grid-icon')[0]
+                .classList.remove('free-grid-iconhidden');
+        };
+
+        const mouseLeave = (e: MouseEvent) => {
+            (<HTMLElement>e.target)
+                .getElementsByClassName('free-grid-icon')[0]
+                .classList.add('free-grid-iconhidden');
+        };
+
         return html`
-            panel
+            ${grouping.map(group => {
+                const click = () => {
+                    this.connector.removeGroup(group);
+                };
+                return html`
+                    <div
+                        @mouseenter=${mouseEnter}
+                        @mouseleave=${mouseLeave}
+                        class="free-grid-grouping-panel-container"
+                    >
+                        <p class="free-grid-grouping-panel-p">
+                            ${group.title || group.field}
+                            <i>
+                                <svg
+                                    @click=${click}
+                                    class="free-grid-icon free-grid-iconhidden"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 16 16"
+                                >
+                                    ${svg`<path d="M3 4l4.3 4L3 12h1.4L8 8.7l3.5 3.3H13L8.6 8 13 4h-1.5L8 7.3 4.4 4H3z"/>`}
+                                </svg></i
+                            >
+                        </p>
+                    </div>
+                `;
+            })}
         `;
     }
 }
