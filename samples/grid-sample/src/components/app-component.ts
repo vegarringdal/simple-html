2e7cc9a962401cb3ac5594c511e8773b615d7f91
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-04-04 23:51:32 +0200

    chore sample

--- samples/grid-sample/src/components/app-component.ts
+++ samples/grid-sample/src/components/app-component.ts
@@ -72,137 +72,175 @@ export default class extends HTMLElement {
 
     public render() {
         return html`
-            <div class="flex flex-col">
-                <free-grid style="height:700px" class="free-grid" .interface=${this.connector}>
-             
-                </free-grid>
-                <div class="m-1">
-                       <button
-                        @click=${() => {
-                            this.clear();
-                        }}
-                    >
-                        clear grouping/sorting etc
-                    </button>
-                    <button
-                        @click=${() => {
-                            this.group();
-                        }}
-                    >
-                        group
-                    </button>
-                </div>
-                <div class="p-1">
-                    <button
-                        @click=${() => {
-                            this.replaceData(100);
-                        }}
-                    >
-                        set 100
-                    </button>
-                    <button
-                        @click=${() => {
-                            this.replaceData(50);
-                        }}
-                    >
-                        set 50
-                    </button>
-                    <button
-                        @click=${() => {
-                            this.replaceData(10);
-                        }}
-                    >
-                        set 10
-                    </button>
-                    <button
-                        @click=${() => {
-                            this.replaceData(1);
-                        }}
-                    >
-                        set 1
-                    </button>
-                    <button
-                        @click=${() => {
-                            this.replaceData(0);
-                        }}
-                    >
-                        set 0
-                    </button>
-                    <button
-                        @click=${() => {
-                            console.log(this.connector.edited());
-                        }}
-                    >
-                        edited
-                    </button>
-                </div>
-                <div class="m-1">
-                    <button
-                        @click=${() => {
-                            this.addData(1);
-                        }}
-                    >
-                        add 1
-                    </button>
-                    <button
-                        @click=${() => {
-                            this.addData(10);
-                        }}
-                    >
-                        add 10
-                    </button>
-                    <button
-                        @click=${() => {
-                            this.addData(100);
-                        }}
-                    >
-                        add 100
-                    </button>
-                    <button
-                        @click=${() => {
-                            this.addData(1000);
-                        }}
-                    >
-                        add 1000
-                    </button>
-                </div>
+            <div class="flex flex-row">
+               
                 <div>
-                    <button
-                        @click=${() => {
-                            this.connector.first();
-                        }}
-                    >
-                        first
-                    </button>
-                    <button
-                        @click=${() => {
-                            this.connector.prev();
-                        }}
-                    >
-                        prev
-                    </button>
-                    <button
-                        @click=${() => {
-                            this.connector.select(5);
-                        }}
-                    >
-                        select row 5
-                    </button>
-                    <button
-                        @click=${() => {
-                            this.connector.next();
-                        }}
-                    >
-                        next
-                    </button>
-                    <button
-                        @click=${() => {
-                            this.connector.last();
-                        }}
-                    >
-                        last
-                    </button>
+                    <div class="m-1 p-1">
+                        <button
+                            @click=${() => {
+                                this.clear();
+                            }}
+                        >
+                            clear grouping/sorting etc
+                        </button>
+                        <button
+                            @click=${() => {
+                                this.group();
+                            }}
+                        >
+                            group
+                        </button>
+                    </div>
+
+                    <div class="m-1 p-1">
+                        <button
+                            @click=${() => {
+                                this.replaceData(100);
+                            }}
+                        >
+                            set 100
+                        </button>
+                        <button
+                            @click=${() => {
+                                this.replaceData(50);
+                            }}
+                        >
+                            set 50
+                        </button>
+                        <button
+                            @click=${() => {
+                                this.replaceData(10);
+                            }}
+                        >
+                            set 10
+                        </button>
+                        <button
+                            @click=${() => {
+                                this.replaceData(1);
+                            }}
+                        >
+                            set 1
+                        </button>
+                        <button
+                            @click=${() => {
+                                this.replaceData(0);
+                            }}
+                        >
+                            set 0
+                        </button>
+                        <button
+                            @click=${() => {
+                                console.log(this.connector.edited());
+                            }}
+                        >
+                            edited
+                        </button>
+                    </div>
+                    <div class="m-1 p-1">
+                        <button
+                            @click=${() => {
+                                this.addData(1);
+                            }}
+                        >
+                            add 1
+                        </button>
+                        <button
+                            @click=${() => {
+                                this.addData(10);
+                            }}
+                        >
+                            add 10
+                        </button>
+                        <button
+                            @click=${() => {
+                                this.addData(100);
+                            }}
+                        >
+                            add 100
+                        </button>
+                        <button
+                            @click=${() => {
+                                this.addData(1000);
+                            }}
+                        >
+                            add 1000
+                        </button>
+                    </div>
+                    <div class="m-1 p-1">
+                        <button
+                            @click=${() => {
+                                this.connector.first();
+                            }}
+                        >
+                            first
+                        </button>
+                        <button
+                            @click=${() => {
+                                this.connector.prev();
+                            }}
+                        >
+                            prev
+                        </button>
+                        <button
+                            @click=${() => {
+                                this.connector.select(5);
+                            }}
+                        >
+                            select row 5
+                        </button>
+                        <button
+                            @click=${() => {
+                                this.connector.next();
+                            }}
+                        >
+                            next
+                        </button>
+                        <button
+                            @click=${() => {
+                                this.connector.last();
+                            }}
+                        >
+                            last
+                        </button>
+                    </div>
+                    <div class="flex flex-col">
+                    <label class="p-1 m-2"
+                        >first:
+                        <input
+                            .value=${this.entity?.first || ''}
+                            @change=${(e: any) => {
+                                if (this.entity && this.entity.first) {
+                                    this.entity.first = e.target.value;
+                                    this.connector.reRender();
+                                }
+                            }}
+                    /></label>
+                    <label class="p-1 m-2"
+                        >last:
+                        <input
+                            .value=${this.entity?.last || ''}
+                            @change=${(e: any) => {
+                                if (this.entity && this.entity.last) {
+                                    this.entity.last = e.target.value;
+                                    this.connector.reRender();
+                                }
+                            }}
+                    /></label>
+                    <label class="p-1 m-2"
+                        >index:
+                        <input
+                            .value=${this.entity?.index || ''}
+                            @change=${(e: any) => {
+                                if (this.entity && this.entity.index) {
+                                    this.entity.index = e.target.value;
+                                    this.connector.reRender();
+                                }
+                            }}
+                    /></label>
                 </div>
+                </div>
+                <free-grid style="height:700px;width:100%" class="free-grid" .interface=${this.connector}>
+                </free-grid>
             </div>
         `;
     }
