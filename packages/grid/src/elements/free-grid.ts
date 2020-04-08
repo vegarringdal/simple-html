a343dd1ceacc8dacb8cbeb3f19883209f86c1cf7
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-04-08 19:25:42 +0200

    fix: we have interface before trying to use it

--- packages/grid/src/elements/free-grid.ts
+++ packages/grid/src/elements/free-grid.ts
@@ -22,7 +22,7 @@ export class FreeGrid extends HTMLElement {
     public connectedCallback() {
         this.resetRowCache();
         this.render();
-        this.currentScrollHeight = this.interface.getScrollVars.__SCROLL_HEIGHT;
+        //this.currentScrollHeight = this.interface.getScrollVars.__SCROLL_HEIGHT;
     }
 
     public disconnectedCallback() {
