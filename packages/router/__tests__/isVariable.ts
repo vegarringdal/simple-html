0d8e97f4f50dabd65ded692aace65ef3605a9567
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-04-10 00:13:34 +0200

    chore: add test - router - isVariable

---
+++ packages/router/__tests__/isVariable.ts
@@ -0,0 +1,16 @@
+import { isVariable } from '../src/isVariable';
+
+describe('isVariable', () => {
+    it('should :name is variable', () => {
+        const result = isVariable(':name');
+        expect(result).toEqual(true);
+    });
+    it('should :name: is not allowed', () => {
+        const result = isVariable(':name:');
+        expect(result).toEqual(false);
+    });
+    it('should name is not varibale', () => {
+        const result = isVariable('name');
+        expect(result).toEqual(false);
+    });
+});
