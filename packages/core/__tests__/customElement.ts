c091358087e7511f6e2449a6514d908054f0891b
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-04-13 17:24:44 +0200

    test: lifecycle customelement

--- packages/core/__tests__/customElement.ts
+++ packages/core/__tests__/customElement.ts
@@ -1,5 +1,4 @@
 import { customElement } from '../src';
-import { promises } from 'dns';
 
 describe('customElement', () => {
     it('renders on connected', (done) => {
@@ -54,6 +53,65 @@ describe('customElement', () => {
                 );
                 done();
             }, 50);
+        });
+    });
+
+    it('check main lifecycle', (done) => {
+        // get decorator function
+        const decorator = customElement('app-root3');
+
+        const events: string[] = [];
+
+        // our element with minimum config
+        const Ele = class extends HTMLElement {
+            data = 'none';
+
+            constructor() {
+                super();
+                events.push('constructor');
+            }
+
+            connectedCallback() {
+                this.data = 'connectedCallback';
+                events.push('connectedCallback');
+            }
+
+            updated() {
+                events.push('updated');
+            }
+
+            render() {
+                events.push('render');
+                return this.data;
+            }
+
+            disconnectedCallback() {
+                events.push('disconnectedCallback');
+            }
+        };
+
+        // trigger decorator
+        decorator(Ele);
+
+        // add custom element
+        document.body.innerHTML = '<app-root3 id="my-element"></app-root3>';
+
+        requestAnimationFrame(() => {
+            //check render
+            expect(document.getElementById('my-element').textContent).toEqual('connectedCallback');
+            // wait for updated to get called
+            requestAnimationFrame(() => {
+                //clear content and check lifecycle
+                document.body.innerHTML = '';
+                requestAnimationFrame(() => {
+                    expect(events[0]).toEqual('constructor');
+                    expect(events[1]).toEqual('connectedCallback');
+                    expect(events[2]).toEqual('render');
+                    expect(events[3]).toEqual('updated');
+                    expect(events[4]).toEqual('disconnectedCallback');
+                    done();
+                });
+            });
         });
     });
 });
