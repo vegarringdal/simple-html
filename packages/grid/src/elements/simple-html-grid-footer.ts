2461603241e0bb2131fff918d384fa6b5059c8aa
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-04-19 21:06:37 +0200

    fix: footer now display rows filtered and total

--- packages/grid/src/elements/simple-html-grid-footer.ts
+++ packages/grid/src/elements/simple-html-grid-footer.ts
@@ -1,17 +1,34 @@
 import { customElement } from '@simple-html/core';
 import { GridInterface } from '../gridInterface';
+import { SimpleHtmlGrid } from './simple-html-grid';
+import { html } from 'lit-html';
 
 @customElement('simple-html-grid-footer')
 export default class extends HTMLElement {
     classList: any = 'simple-html-grid-footer';
     connector: GridInterface;
+    ref: SimpleHtmlGrid;
 
     connectedCallback() {
         const config = this.connector.config;
         this.style.height = config.footerHeight + 'px';
+        this.ref.addEventListener('reRender', this);
+    }
+
+    disconnectedCallback() {
+        this.ref.removeEventListener('reRender', this);
+    }
+
+    handleEvent(e: any) {
+        if (e.type === 'reRender') {
+            this.render();
+        }
     }
 
     render() {
-        return 'footer';
+        const totalRows = this.connector.completeDataset.length;
+        const filter = this.connector.filteredDataset.length;
+
+        return html`<div style="text-align:center">${filter}/${totalRows}</div>`;
     }
 }
