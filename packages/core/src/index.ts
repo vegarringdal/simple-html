e5d3989577cf5ca890b81477e784f8000c8e8fa4
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-12-31 10:46:54 +0100

    fix: export correct classes

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,14 @@ export { attribute } from './attribute';
 export { property } from './property';
 export { customElement } from './customElement';
 export { publish, subscribe, unSubscribe, publishNext, publishSync } from './transmitter';
-export { validateKey, stateResult, State, GLOBAL_STATE_EVENT } from './state';
+export {
+    validateKey,
+    stateResult,
+    ObjectState,
+    SimpleState,
+    ObjectStateInternal,
+    SimpleStateInternal,
+    GLOBAL_STATE_EVENT
+} from './state';
 export { requestRender } from './requestRender';
 export { disconnectedCallback } from './disconnectedCallback';
