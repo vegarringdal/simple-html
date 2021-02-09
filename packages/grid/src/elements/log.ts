3e44f07b925a0e52f6b11ba3924a6ab258694994
Vegar Ringdal <vegar.ringdal@gmail.com>
2021-02-09 19:37:14 +0100

    chore: footer

--- packages/grid/src/elements/log.ts
+++
@@ -1,6 +0,0 @@
-const logger = false;
-
-// helper to see if we have events going in circles
-export function log(element: any, e: any) {
-    if (logger) console.log('logger:', element?.nodeName, e?.type);
-}
