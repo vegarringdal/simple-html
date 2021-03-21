937b3a620182e756240f12f049eeb128680ca23e
Vegar Ringdal <vegar.ringdal@gmail.com>
2021-03-21 14:27:31 +0100

    fix(ds): new prop on entity needs to be set first of all

--- packages/datasource/src/entity.ts
+++ packages/datasource/src/entity.ts
@@ -5,12 +5,12 @@ import { Entity } from './types';
  */
 
 export class EntityHandler {
-    __editedProps? = {};
-    __originalValues? = {};
-    __currentValues? = {};
-    __newprops? = {};
-    __isNew? = false;
-    __edited? = false;
+    __editedProps?= {};
+    __originalValues?= {};
+    __currentValues?= {};
+    __newprops?= {};
+    __isNew?= false;
+    __edited?= false;
     __controller?: EntityHandler;
     __KEY?: string | number;
     __KEYSTRING?: string | number;
@@ -75,60 +75,60 @@ export class EntityHandler {
         }
 
         if (update) {
-            if (obj.hasOwnProperty(prop)) {
-                if (!this.__editedProps.hasOwnProperty(prop) && !this.__isNew) {
-                    this.__originalValues[prop] = obj[prop];
-                    this.__editedProps[prop] = true;
-                } else {
-                    this.__editedProps[prop] = true;
-                }
-                // if user just set back to original value we want to remove the "edited" part
-                let _original = this.__originalValues[prop];
-                let _value = value;
+            if (!this.__newprops.hasOwnProperty(prop)) {
+                this.__originalValues[prop] = obj[prop];
+                this.__newprops[prop] = true;
+            }
 
-                // if date, clear the "timezone/time part"
-                if (_original instanceof Date) {
-                    try {
-                        _original = new Date(
-                            new Date(_original).getFullYear(),
-                            new Date(_original).getMonth(),
-                            new Date(_original).getDate(),
-                            0,
-                            0,
-                            0,
-                            0
-                        );
-                    } catch (e) {
-                        _original = null;
-                    }
-                }
-                if (_value instanceof Date) {
-                    _value = new Date(
-                        new Date(_value).getFullYear(),
-                        new Date(_value).getMonth(),
-                        new Date(_value).getDate(),
+
+            if (!this.__editedProps.hasOwnProperty(prop) && !this.__isNew) {
+                this.__originalValues[prop] = obj[prop];
+                this.__editedProps[prop] = true;
+            } else {
+                this.__editedProps[prop] = true;
+            }
+            // if user just set back to original value we want to remove the "edited" part
+            let _original = this.__originalValues[prop];
+            let _value = value;
+
+            // if date, clear the "timezone/time part"
+            if (_original instanceof Date) {
+                try {
+                    _original = new Date(
+                        new Date(_original).getFullYear(),
+                        new Date(_original).getMonth(),
+                        new Date(_original).getDate(),
                         0,
                         0,
                         0,
                         0
                     );
-                }
-
-                if (
-                    _original === _value ||
-                    ((_original === null || _original === undefined) && _value === '')
-                ) {
-                    this.__editedProps[prop] = false;
-                }
-
-                this.__currentValues[prop] = value;
-                obj[prop] = value;
-            } else {
-                if (!this.__newprops.hasOwnProperty(prop)) {
-                    this.__originalValues[prop] = obj[prop];
-                    this.__newprops[prop] = true;
+                } catch (e) {
+                    _original = null;
                 }
             }
+            if (_value instanceof Date) {
+                _value = new Date(
+                    new Date(_value).getFullYear(),
+                    new Date(_value).getMonth(),
+                    new Date(_value).getDate(),
+                    0,
+                    0,
+                    0,
+                    0
+                );
+            }
+
+            if (
+                _original === _value ||
+                ((_original === null || _original === undefined) && _value === '')
+            ) {
+                this.__editedProps[prop] = false;
+            }
+
+            this.__currentValues[prop] = value;
+            obj[prop] = value;
+
             this.__edited = Object.values(this.__editedProps).includes(true);
             this.__currentValues[prop] = value
             obj[prop] = value;
