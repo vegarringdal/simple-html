1a80a419031eb8c16e8d3c8aad360b274735c6e0
Vegar Ringdal <vegar.ringdal@gmail.com>
2021-04-04 16:10:03 +0200

    chore: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,6 @@ const { off } = require('process');
 module.exports = {
     parser: '@typescript-eslint/parser',
     extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        project: './tsconfig.eslint.json',
-        tsconfigRootDir: './',
-        warnOnUnsupportedTypeScriptVersion: false
-    },
     rules: {
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
