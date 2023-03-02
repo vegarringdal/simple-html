1afeb62cdcb96f2921cb8628245ebdb9c1e1c519
Vegar Ringdal <vegar.ringdal@gmail.com>
2023-03-02 20:08:19 +0100

    fix: prettyprintstring

--- packages/grid/src/grid/gridFunctions/prettyPrintString.ts
+++ packages/grid/src/grid/gridFunctions/prettyPrintString.ts
@@ -6,7 +6,14 @@
 export function prettyPrintString(text: string) {
     const prettytext = text
         .split('_')
-        .map((e) => e[0].toUpperCase() + e.substring(1, e.length).toLowerCase())
-        .join(' ');
+        .map((e) => {
+            if (e) {
+                return e[0].toUpperCase() + e.substring(1, e.length).toLowerCase();
+            } else {
+                return '';
+            }
+        })
+        .join(' ')
+        .replaceAll('  ', ' ');
     return prettytext;
 }
